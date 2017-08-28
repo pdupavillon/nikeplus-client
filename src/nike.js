8d8aa3c87f178a323c7cb4f543df68317eeed4ca
Paul du Pavillon <p.dupavillon@gmail.com>
2017-08-28 10:56:27 -0500

    Update uri

--- src/nike.js
+++ src/nike.js
@@ -138,7 +138,7 @@ export default class NikeClient {
         `<?xml version="1.0" encoding="UTF-8"?>
             <gpx
             version="1.1"
-            creator="Paul du Pavillon - https://nikeplus.bullrox.net"
+            creator="Paul du Pavillon - https://nike.bullrox.net"
             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
             xmlns="http://www.topografix.com/GPX/1/1"
             xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
